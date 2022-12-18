898e72f13859b44fd521eaae10b3b646f45e4732
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2022-12-18 19:25:01 +0100

    Added image display

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+    list.insertAdjacentHTML(
+        "beforeend",
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img src="${image.preview}" alt="${image.description}" class="gallery__image" data-source="${image.original}"/>
+            </a>
+        </div>`
+    );
+});
